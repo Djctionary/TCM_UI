1a38bd74200f5f7a3608be486d8de696bd6e2fea
ZhangQi <2801038431@qq.com>
2024-02-27 18:03:47 +0800

    增加诊断记录界面

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 // Ensure the script runs after the document is fully loaded
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Get the input element by ID
     var inputElement = document.getElementById('myInput');
 
     ID = localStorage.getItem('myInput');
-    
+
     inputElement.value = ID;
+
+    var boxText1 = document.getElementById('boxText1');
+    var boxText2 = document.getElementById('boxText2');
+    var boxText3 = document.getElementById('boxText3');
+    var boxText4 = document.getElementById('boxText4');
+
+    Box1 = localStorage.getItem('boxText1');
+    Box2 = localStorage.getItem('boxText2');
+    Box3 = localStorage.getItem('boxText3');
+    Box4 = localStorage.getItem('boxText4');
+
+    boxText1.value = Box1;
+    boxText2.value = Box2;
+    boxText3.value = Box3;
+    boxText4.value = Box4;
 });
 
 function focusOnInput() {
@@ -14,7 +29,7 @@ function focusOnInput() {
 
     // 将焦点设置到输入框上
     inputElement.focus();
-  }
+}
 
 var bar = document.getElementById("bar");
 
@@ -41,11 +56,11 @@ function handleClick1() {
     faceRecognition.style.cursor = "pointer";
 
     var inputElement = document.getElementById('myInput');
-    
+
     var inputValue = inputElement.value;
-    
-    localStorage.setItem('myInput',(inputValue));
-    
+
+    localStorage.setItem('myInput', (inputValue));
+
     // 触发超链接跳转
     window.location.href = "record.html?source=" + inputValue + "_面部识别";
 }
@@ -60,17 +75,17 @@ var faceRecognition = document.getElementById("舌相抓取");
 // 点击事件处理函数
 function handleClick2() {
     // 移除可能已有的 animate 类
-    faceRecognition.classList.remove("animate");    
+    faceRecognition.classList.remove("animate");
 
     // 设置鼠标样式为手型   
     faceRecognition.style.cursor = "pointer";
 
     var inputElement = document.getElementById('myInput');
-    
+
     var inputValue = inputElement.value;
-    
-    localStorage.setItem('myInput',(inputValue));
-    
+
+    localStorage.setItem('myInput', (inputValue));
+
     // 触发超链接跳转
     window.location.href = "record.html?source=" + inputValue + "_舌相抓取";
 }
@@ -91,13 +106,13 @@ function handleClick3() {
     faceRecognition.style.cursor = "pointer";
 
     var inputElement = document.getElementById('myInput');
-    
+
     var inputValue = inputElement.value;
-    
-    localStorage.setItem('myInput',(inputValue));
-    
+
+    localStorage.setItem('myInput', (inputValue));
+
     // 触发超链接跳转
-    window.location.href = "record.html?source=" + inputValue +"_步态记录";
+    window.location.href = "record.html?source=" + inputValue + "_步态记录";
 }
 
 // 为步态记录元素添加点击事件监听器
@@ -116,14 +131,39 @@ function handleClick4() {
     faceRecognition.style.cursor = "pointer";
 
     var inputElement = document.getElementById('myInput');
-    
+
     var inputValue = inputElement.value;
-    
-    localStorage.setItem('myInput',(inputValue));
-    
+
+    localStorage.setItem('myInput', (inputValue));
+
     // 触发超链接跳转
     window.location.href = "record.html?source=" + inputValue + "_患者自述";
 }
 
 // 为患者自述元素添加点击事件监听器
 faceRecognition.addEventListener("click", handleClick4);
+
+
+
+
+var boxText1 = document.getElementById('boxText1');
+var boxText2 = document.getElementById('boxText2');
+var boxText3 = document.getElementById('boxText3');
+var boxText4 = document.getElementById('boxText4');
+
+function inputBoxStorage(event) {
+
+    var name = event.id
+    // console.log(name);
+
+    var content = event.value;
+    // console.log(content);
+    
+    localStorage.setItem(name, content);
+}
+
+
+boxText1.addEventListener('change', (event) => inputBoxStorage(boxText1))
+boxText2.addEventListener('change', (event) => inputBoxStorage(boxText2))
+boxText3.addEventListener('change', (event) => inputBoxStorage(boxText3))
+boxText4.addEventListener('change', (event) => inputBoxStorage(boxText4))
